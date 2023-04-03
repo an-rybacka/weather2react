a139be47ed32aa8639b6c5ca4d9dfc7cff96fff6
an-rybacka <121604351+an-rybacka@users.noreply.github.com>
2023-04-04 01:21:45 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
-import SearchEngine from "./SearchEngine"
+import SearchEngine from "./SearchEngine";
+import "bootstrap/dist/css/bootstrap.css";
+
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
-<SearchEngine />
+        <SearchEngine />
       </div>
       <footer>
         This project was coded by Aneta Rybacka with{" "}
-        <a href="https://react.dev/" target="_blank">
+        <a href="https://react.dev/" target="_blank" rel="noreferrer">
           React
         </a>{" "}
         and is{" "}
-        <a href="https://github.com/an-rybacka/weather-react" target="_blank">
+        <a
+          href="https://github.com/an-rybacka/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on GitHub
         </a>{" "}
       </footer>
